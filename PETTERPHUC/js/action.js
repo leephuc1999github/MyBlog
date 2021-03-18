00ed8a881ab162d578c8059b39af15eccb9d8419
phuclk <phuclk@tandan.com.vn>
2021-03-18 11:26:23 +0700

    create side bar left

--- PETTERPHUC/js/action.js
+++ PETTERPHUC/js/action.js
@@ -4,49 +4,42 @@ const my_blog = document.getElementById("my-blog");
 const my_work = document.getElementById("my-work");
 const my_resume = document.getElementById("my-resume");
 const bar = document.getElementById("side-bar-toggle");
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        goToPage("about");
+    }
+};
 
-function goToPage(name , item){
-    switch(name){
+function goToPage(name) {
+    about_me.classList.add("hidden");
+    contact_me.classList.add("hidden");
+    my_blog.classList.add("hidden");
+    my_work.classList.add("hidden");
+    my_resume.classList.add("hidden");
+    switch (name) {
         case "about":
             about_me.classList.remove("hidden");
-            contact_me.classList.add("hidden");
-            my_blog.classList.add("hidden");
-            my_work.classList.add("hidden");
-            my_resume.classList.add("hidden");
             break;
         case "contact":
             contact_me.classList.remove("hidden");
-            about_me.classList.add("hidden");
-            my_blog.classList.add("hidden");
-            my_work.classList.add("hidden");
-            my_resume.classList.add("hidden");
             break;
         case "blog":
-            contact_me.classList.add("hidden");
-            about_me.classList.add("hidden");
             my_blog.classList.remove("hidden");
-            my_work.classList.add("hidden");
-            my_resume.classList.add("hidden");
             break;
         case "work":
-            contact_me.classList.add("hidden");
-            about_me.classList.add("hidden");
-            my_blog.classList.add("hidden");
             my_work.classList.remove("hidden");
-            my_resume.classList.add("hidden");
             break;
         default:
-            contact_me.classList.add("hidden");
-            about_me.classList.add("hidden");
-            my_blog.classList.add("hidden");
-            my_work.classList.add("hidden");
             my_resume.classList.remove("hidden");
             break;
     }
+    closeSideBar();
 }
-function showSideBar(){
+
+function showSideBar() {
     bar.classList.remove("hidden");
 }
-function closeSideBar(){
+
+function closeSideBar() {
     bar.classList.add("hidden");
 }
